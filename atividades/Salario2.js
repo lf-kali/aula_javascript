45796195ef9355e8931894e30e5ebccf770415bc
kali <cbjk.corp@gmail.com>
2025-10-30 15:34:47 -0300

    Entrega da segunda atividade sobre salário

---
+++ atividades/Salario2.js
@@ -0,0 +1,11 @@
+import readline from 'readline-sync';
+
+let salarioBruto = readline.questionInt(`Digite seu salário Bruto: `);
+let adicNoturno = readline.questionInt(`Digite o adicional noturno: `);
+let horasExtras = readline.questionInt(`Digite quanto em horas extras: `);
+let descontos = readline.questionInt(`Digite os descontos: `);
+
+let salarioLiquido = salarioBruto + adicNoturno + horasExtras - descontos;
+
+console.log(`Salário Líquido: ${salarioLiquido}`)
+
